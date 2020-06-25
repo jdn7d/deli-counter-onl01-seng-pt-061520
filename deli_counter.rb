4e24fcd92e1b4f98e93976378ab651d039f14542
Jessie Donovan <donovanajessie@gmail.com>
2020-06-25 01:09:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ puts  "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
-if katz_deli.length => 1
+if katz_deli.length >= 1
   puts "Currently serving #{katz_deli.first}."
     katz_deli.shift
 else
